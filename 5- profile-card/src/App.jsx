baaa7f1050fe9bac4637702bd10a6b14d3b5cdf4
Rameez <ulhassanr59@gmail.com>
2024-12-24 22:48:56 +0500

    profile card in react

---
+++ 5- profile-card/src/App.jsx
@@ -0,0 +1,15 @@
+import Profile from "../components/Profile"
+import ProfileCard from "../components/ProfileCard"
+
+
+function App() {
+  
+
+  return (
+    <>
+    <Profile/>
+    </>
+  )
+}
+
+export default App
